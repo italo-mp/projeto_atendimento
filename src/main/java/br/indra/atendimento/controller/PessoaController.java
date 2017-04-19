caf9e0a9ec65d7423219b50ee904fbed8acb2750
italo-mp <i-mp1@hotmail.com>
2017-04-19 10:03:29 -0300

    Inserindo metodo de exclusao e arrumando o retorno

--- src/main/java/br/indra/atendimento/controller/PessoaController.java
+++ src/main/java/br/indra/atendimento/controller/PessoaController.java
@@ -2,9 +2,13 @@ package br.indra.atendimento.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.indra.atendimento.model.Pessoa;
 import br.indra.atendimento.service.PessoaService;
@@ -25,8 +29,24 @@ public class PessoaController {
 	}
 
 	@PostMapping
-	public ModelAndView salvarClientes(Pessoa pessoa) {
-		pessoaService.salvarPessoa(pessoa);
-		return clientes();
+	public String salvarClientes(Pessoa pessoa, BindingResult br, RedirectAttributes ra) {
+		if (pessoaService.salvarPessoa(pessoa)) {
+			ra.addFlashAttribute("mensagemSucesso", "Cliente excluído com sucesso!");
+		} else {
+			ra.addFlashAttribute("mensagemErro", "Não foi possível excluir cliente");
+		}
+
+		return "redirect:/clientes";
 	}
+
+	@GetMapping(value = "/excluir/{id}")
+	public String excluirCliente(@PathVariable Long id, RedirectAttributes ra) {
+		if (pessoaService.excluirPessoa(id)) {
+			ra.addFlashAttribute("mensagemSucesso", "Cliente excluído com sucesso!");
+		} else {
+			ra.addFlashAttribute("mensagemErro", "Não foi possível excluir cliente");
+		}
+		return "redirect:/clientes";
+	}
+
 }
